e25ab5f753fac790ab45a104c90a72022fd5f279
Yusuke <theshoreunder@gmail.com>
2025-02-09 11:00:14 +0900

    Done chapter 6

---
+++ chapter6/menu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while true
+do
+    clear
+    echo "Choose an item: a, b, or c"
+    echo "a: Backup"
+    echo "b: Display Calendar"
+    echo "c: Exit"
+    # read -sn1
+    case "$REPLY" in
+        a) tar -czvf $HOME/backup.tgz ${HOME}/bin;;
+        b) cal;;
+        c) exit 0;;
+    esac
+    read "REPLY?Press any key to continue: "
+done
